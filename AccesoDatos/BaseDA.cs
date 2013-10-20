bd717993b93fd264f32f693e196433c5683713dc
luisystem <kuisystem@bolivia.com>
2013-10-20 10:44:58 -0400

    Capa Acceso a Datos

---
+++ AccesoDatos/BaseDA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class BaseDA
+    {
+        protected DBDeportesDataContext GetDC()
+        {
+            DBDeportesDataContext DC = new DBDeportesDataContext();
+            DC.DeferredLoadingEnabled = false;
+            return DC;
+        }
+        protected List<T> ConsultaALista<T>(IQueryable<T> query)
+        {
+            List<T> lista = new List<T>();
+            foreach(T item in query)
+            {
+                lista.Add(item);
+            }
+            return lista;
+        }    
+    }
+}
