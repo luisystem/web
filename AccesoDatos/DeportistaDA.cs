8e8146d8f756179597ff004e51cbd91ea40ea4f6
luisystem <kuisystem@bolivia.com>
2013-10-20 21:23:07 -0400

    Tabla Deportista

---
+++ AccesoDatos/DeportistaDA.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class DeportistaDA : BaseDA, IDataAccess<deportista>
+    {
+         #region Miembros de IDataAccess<Deportista>
+            public List<deportista> GetAll()
+            {
+                DBDeportesDataContext dc = GetDC();
+                IQueryable<deportista> query = from deportista in dc.deportista
+                                               select deportista;
+                return ConsultaALista<deportista>(query);
+            }
+            public void Adicionar(deportista item)
+            {
+                DBDeportesDataContext dc = GetDC();
+                dc.deportista.InsertOnSubmit(item);
+                dc.SubmitChanges();
+            }
+            public void Eliminar(deportista item)
+            {
+                DBDeportesDataContext dc = GetDC();
+                dc.deportista.Attach(item);
+                dc.deportista.DeleteOnSubmit(item);
+                dc.SubmitChanges();
+            }
+            public void Actualizar(deportista nuevoItem, deportista originalItem)
+            {
+                DBDeportesDataContext dc = GetDC();
+                dc.deportista.Attach(nuevoItem, originalItem);
+                dc.SubmitChanges();
+            }
+            #endregion
+
+        }
+    }
+
---
+++ AccesoDatos/IDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public interface IDataAccess<T>
+    {
+        List<T> GetAll();
+        void Adicionar(T item);
+        void Eliminar(T item);
+        void Actualizar(T nuevoItem, T originalItem);
+    }
+}
