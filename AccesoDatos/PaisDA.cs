6061af7da4a82e3fa09fd81df0ecd0f74e64fc95
luisystem <luisystem@bolivia.com>
2013-10-21 10:43:56 -0400

    Pais DA

---
+++ AccesoDatos/PaisDA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class PaisDA:BaseDA,IDataAccess<Pais>
+    {
+        #region Miembros de IDataAccess<pais>
+        public List<Pais> GetAll()
+        {
+            DBDeportesDataContext dc = GetDC();
+            IQueryable<Pais> query = from pais in dc.Pais
+                                     select pais;
+            return ConsultaALista<Pais>(query);
+        }
+        #endregion
+    }
+}
